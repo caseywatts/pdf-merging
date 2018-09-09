799e8c1cd5a89e2da989d8fe040b81567f9bb9fd
Casey Watts <casey.s.watts@gmail.com>
2018-09-09 13:24:20 -0400

    get it working with ghostscript

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
-https://github.com/metaist/pdfmerge
-
+using [pdfmerge](https://github.com/metaist/pdfmerge)
 ```
 brew install python@2
 pip install --user pdfmerge
+```
+
+using the ancient ghostscript library
+```
+brew install gs
 ```
--- script.js
+++ script.js
@@ -1,19 +1,36 @@
 const shell = require('shelljs');
-const inputFiles = shell.find('.').filter(function(file) {
-  return file.match(/\.pdf$/) && !file.match(/.*Score.*/);
-});
 
-inputFilesWithDuplicates = inputFiles.reduce((collection, file) => {
-  doXTimes(4, function() {
-    collection.push(file)
-  })
-  return collection;
-}, [])
 
-console.log(inputFilesWithDuplicates)
-const listOfFiles = inputFiles.map((f) => `"${f}"`).join(' ');
+const inputFiles = gatherInputFiles();
+const inputFilesWithDuplicates = duplicateFilesAppropriately(inputFiles)
+const duplicatedFilenamesString = joinFilenames(inputFilesWithDuplicates)
+mergePDFs(duplicatedFilenamesString)
 
-shell.exec(`gs -dNOPAUSE -sDEVICE=pdfwrite -sOUTPUTFILE=combine.pdf -dBATCH ${listOfFiles}`)
+
+function duplicateFilesAppropriately(inputFiles) {
+  return inputFiles.reduce((collection, file) => {
+    doXTimes(4, function() {
+      collection.push(file)
+    })
+    return collection;
+  }, [])
+}
+
+function gatherInputFiles() {
+  return shell.find('.').filter(function(file) {
+    return file.match(/\.pdf$/) && !file.match(/.*Score.*/)&& !file.match(/.*output.*/);
+  });
+}
+
+function joinFilenames(files) {
+  return files.map((f) => `"${f}"`).join(' ');
+}
+
+function mergePDFs(pdfs) {
+  const command = `gs -dNOPAUSE -sDEVICE=pdfwrite -sOUTPUTFILE=output.pdf -dBATCH ${pdfs}`;
+  console.log(command)
+  shell.exec(command)
+}
 
 function doXTimes(xTimes, f) {
   Array.from(Array(xTimes)).forEach(() => {
